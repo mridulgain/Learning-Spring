27859cfea4073275928af7fb8492f06c9240dea1
Mridul Gain <mridulgain@gmail.com>
2018-12-29 12:24:33 +0530

    Sprng core hello world

---
+++ firstspring/src/firstproject/HelloWorld.java
@@ -0,0 +1,10 @@
+package firstproject;
+public class HelloWorld{
+	private String message;
+	public void setMessage(String message) {
+		this.message = message;	
+	}
+	public String getMessage() {
+		return this.message;
+	}
+}
